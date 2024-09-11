fdfcd694c23cfd84bd2b16533dd74b2eaad0ada4
Oyebode <preckidtech@gmail.com>
2024-09-11 12:56:20 +0100

    update

---
+++ js/blog.js
@@ -0,0 +1,12 @@
+/** @format */
+
+// // /** @format */
+
+// // const presentBlog = document.getElementById("present-blog");
+
+// // presentBlog.textConten
+// const seoWebsiteBlogDate = document.getElementById("seo-website-blog-date");
+// const seoWebsiteBlog = document.getElementById("seo-website-blog");
+
+// // seoWebsiteBlogDate.textContent =
+// seoWebsiteBlog.textContent = `How to Optimize Your Website for Search Engines (SEO)`;
